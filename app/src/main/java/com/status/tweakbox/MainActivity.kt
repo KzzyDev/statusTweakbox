35b2911136bc336dfca0c48f19e7f7024a39f86e
KzzyDev <mat.kzzy@gmail.com>
2026-01-13 23:24:51 -0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/status/tweakbox/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.webviewtemplate.webviewtemplate
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                injectJS(view)
+            }
+        }
+
+        // 🔒 URL fija
+        webView.loadUrl("https://status.cafe/add")
+    }
+
+    private fun injectJS(webView: WebView?) {
+        val js = assets.open("script.js")
+            .bufferedReader()
+            .use { it.readText() }
+
+        webView?.evaluateJavascript(js, null)
+    }
+}
